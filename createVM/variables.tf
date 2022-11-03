4c34a733728c905a0a5a4a3de4f0deab0a370c8e
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-03 15:58:22 -0400

    Create variables.tf

---
+++ createVM/variables.tf
@@ -0,0 +1,41 @@
+variable "resource_group_name" {
+  description = "RG name"
+  type = string
+  default = ""
+}
+
+variable "location" {
+  description = "RG location"
+  type = string
+  default = "eastus"
+}
+
+variable "virtual_network_name" {
+  description = "VN name"
+  type = string
+  default = ""
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type = string
+  default = ""
+}
+
+variable "nic_name" {
+  description = "NIC name"
+  type = string
+  default = ""
+}
+
+variable "virtual_machine_name" {
+  description = "VM name"
+  type = string
+  default = ""
+}
+
+variable "admin_username" {
+  description = "Admin username"
+  type = string
+  default = ""
+}
