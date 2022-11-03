bf507aa0ce11f00749226a48706dadb8923e6515
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-03 15:57:26 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+module "create-virtual-machine" {
+  source = "./createVM"
+
+  resource_group_name  = "terraformRG"
+  location             = "eastus"
+  virtual_network_name = "terraformVN"
+  subnet_name          = "terraformSubnet"
+  nic_name             = "terraformNIC"
+  virtual_machine_name = "FirstVM"
+  admin_username       = "testuser"
+}
+
+module "create-virtual-machine2" {
+  source = "./createVM"
+
+  resource_group_name  = "terraformRG2"
+  location             = "eastus"
+  virtual_network_name = "terraformVN2"
+  subnet_name          = "terraformSubnet2"
+  nic_name             = "terraformNIC2"
+  virtual_machine_name = "SecondVM"
+  admin_username       = "testuser"
+}
