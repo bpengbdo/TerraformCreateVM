704d5e0088cd0ecb2a59fa0fe458aa667ba4980c
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-07 16:02:35 -0500

    Update createVM.tf

--- createVM/createVM.tf
+++ createVM/createVM.tf
@@ -19,29 +19,17 @@ resource "tls_private_key" "example_ssh" {
   rsa_bits  = 4096
 }
 
-resource "azurerm_resource_group" "terraformRG" {
-  name = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "terraformVN" {
-  name                = var.virtual_network_name
-  address_space       = ["10.0.0.0/16"]
-  resource_group_name = azurerm_resource_group.terraformRG.name
-  location            = azurerm_resource_group.terraformRG.location
-}
-
 resource "azurerm_subnet" "terraformSubnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.terraformRG.name
-  virtual_network_name = azurerm_virtual_network.terraformVN.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_network_interface" "terraformNIC" {
   name                = var.nic_name
-  resource_group_name = azurerm_resource_group.terraformRG.name
-  location            = azurerm_resource_group.terraformRG.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 
   ip_configuration {
     name                          = "terraformIP"
@@ -52,8 +40,8 @@ resource "azurerm_network_interface" "terraformNIC" {
 
 resource "azurerm_linux_virtual_machine" "terraformVM" {
   name                = var.virtual_machine_name
-  resource_group_name = azurerm_resource_group.terraformRG.name
-  location            = azurerm_resource_group.terraformRG.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   size                = "Standard_DS1_v2"
   network_interface_ids = [
     azurerm_network_interface.terraformNIC.id,
