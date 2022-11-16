f879575fb520c2367b9ca4268e78e5d4f1c1fcb9
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-16 12:34:40 -0500

    Update createVM.tf

--- createVM/createVM.tf
+++ createVM/createVM.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "terraformSubnet" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
-  address_prefixes     = [var.address_prefixes]
+  address_prefixes     = var.address_prefixes
 }
 
 resource "azurerm_network_interface" "terraformNIC" {
