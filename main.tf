6268bbb1c729ac20779b7c5c020b7335d70c54b9
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-12-05 23:51:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 module "create-virtual-machine" {
     source = "./createVM"
+    
+    resource_group_name  = var.resource_group_name
+    location             = var.location
+    virtual_network_name = var.virtual_network_name
+    subnet_name          = var.subnet_name
+    nic_name             = var.nic_name
+    virtual_machine_name = var.virtual_machine_name
+    admin_username       = var.admin_username
+    address_prefixes     = var.address_prefixes
 }
