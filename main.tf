b648ef4558debe0aba9e705fda6d9c19e3dfcc96
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-12-05 23:16:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 module "create-virtual-machine" {
     source = "./createVM"
-
-    resource_group_name  = "terraformRG"
-    location             = "eastus"
-    virtual_network_name = "terraformVN"
-    subnet_name          = "terraformSubnet"
-    nic_name             = "terraformNIC"
-    virtual_machine_name = "MyVM"
-    admin_username       = "testuser"
-    address_prefixes     = ["10.0.0.0/24"]
 }
