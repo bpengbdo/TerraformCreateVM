238a4c5c186a7b630ec25ef35eb4d3090aeee2cd
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-12-05 23:16:32 -0500

    Update variables.tf

--- createVM/variables.tf
+++ createVM/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "RG name"
   type = string
-  default = ""
+  default = "terraformRG"
 }
 
 variable "location" {
@@ -13,36 +13,36 @@ variable "location" {
 variable "virtual_network_name" {
   description = "VN name"
   type = string
-  default = ""
+  default = "terraformVN"
 }
 
 variable "subnet_name" {
   description = "Subnet name"
   type = string
-  default = ""
+  default = "terraformSubnet"
 }
 
 variable "nic_name" {
   description = "NIC name"
   type = string
-  default = ""
+  default = "terraformNIC"
 }
 
 variable "virtual_machine_name" {
   description = "VM name"
   type = string
-  default = ""
+  default = "MyVM"
 }
 
 variable "admin_username" {
   description = "Admin username"
   type = string
-  default = ""
+  default = "testuser"
 }
 
 variable "address_prefixes" {
   description = "Address prefixes"
   type = list(string)
-  default = []
+  default = ["10.0.0.0/24"]
 }
 
