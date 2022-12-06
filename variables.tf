28cb02cc04b34bf36f242a44e0b5c70d8b2fd09d
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-12-05 23:59:21 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "resource_group_name" {
+  description = "RG name"
+  type = string
+  default = "terraformRG"
+}
+
+variable "location" {
+  description = "RG location"
+  type = string
+  default = "eastus"
+}
+
+variable "virtual_network_name" {
+  description = "VN name"
+  type = string
+  default = "terraformVN"
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type = string
+  default = "terraformSubnet"
+}
+
+variable "nic_name" {
+  description = "NIC name"
+  type = string
+  default = "terraformNIC"
+}
+
+variable "virtual_machine_name" {
+  description = "VM name"
+  type = string
+  default = "MyVM"
+}
+
+variable "admin_username" {
+  description = "Admin username"
+  type = string
+  default = "testuser"
+}
+
+variable "address_prefixes" {
+  description = "Address prefixes"
+  type = list(string)
+  default = ["10.0.0.0/24"]
+}
